9de270f9cec1ed75c7b7784c2ebda73b13dc9155
rhoffmanssf <rhoffman@softstackfactory.org>
2016-03-08 00:28:47 +0000

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,30 @@
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+  $stateProvider
+  .state('landing', {
+    url: '/',
+    templateUrl: 'templates/landing.html',
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+  })
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+  });
+});
